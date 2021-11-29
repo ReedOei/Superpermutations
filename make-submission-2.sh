f2707cb7caab404f5cca28d44716a312b1e4d3ca
Reed Oei <oei.reed@gmail.com>
2021-11-28 16:03:03 -0800

    Add some utility stuff

--- make-submission-2.sh
+++ make-submission-2.sh
@@ -10,6 +10,6 @@ done
 
 (
     echo "schedule"
-    cat sol | sed -E "s/result Config: < [0-9]+ \||>//g" | sed -E "s/1/🎅/g" | sed -E "s/2/🤶/g" | sed -E "s/3/🦌/g" | sed -E "s/4/🧝/g" | sed -E "s/5/🎄/g" | sed -E "s/6/🎁/g" | sed -E "s/7/🎀/g"
-) # > "$output_file"
+    cat sol | sed -E "s/result Config: < [0-9]+ \||[<()>, ]//g" | sed -E "s/;/\n/g" | sed -E "s/1/🎅/g" | sed -E "s/2/🤶/g" | sed -E "s/3/🦌/g" | sed -E "s/4/🧝/g" | sed -E "s/5/🎄/g" | sed -E "s/6/🎁/g" | sed -E "s/7/🎀/g"
+) > "$output_file"
 
